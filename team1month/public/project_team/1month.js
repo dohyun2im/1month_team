8cb700c4a80559baf6b3594bd23cf850400a29ff
504 <504@DESKTOP-ECPN2C5>
2022-08-29 18:10:49 +0900

    월요일 마지막

--- team1month/public/project_team/1month.js
+++ team1month/public/project_team/1month.js
@@ -1,3 +1,65 @@
-/**
- * 
- */
+let header = document.querySelector("header").offsetTop;
+let sec1 = document.querySelector(".one").offsetTop;
+let sec2 = document.querySelector(".two").offsetTop;
+let sec3 = document.querySelector(".three").offsetTop;
+let sec4 = document.querySelector(".four").offsetTop;
+let sec5 = document.querySelector(".five").offsetTop;
+let sec6 = document.querySelector(".six").offsetTop;
+let sec7 = document.querySelector(".seven").offsetTop;
+let sec8 = document.querySelector(".eight").offsetTop;
+
+let img1 = document.querySelector("#img1")
+let img2 = document.querySelector("#img2")
+let img3 = document.querySelector("#img3")
+let img4 = document.querySelector("#img4")
+let img5 = document.querySelector("#img5")
+let img6 = document.querySelector("#img6")
+let img7 = document.querySelector("#img7")
+let img8 = document.querySelector("#img8")
+let img9 = document.querySelector("#main")
+let img10 = document.querySelector("#back")
+
+img1.addEventListener('click' , () =>{
+	window.scrollTo({top:sec1, behavior:'smooth'});
+	img1.style.borderColor = "#f5c181";
+})
+
+img2.addEventListener('click' , () =>{
+	window.scrollTo({top:sec2, behavior:'smooth'});
+	img2.style.borderColor = "#f5c181";
+})
+
+img3.addEventListener('click' , () =>{
+	window.scrollTo({top:sec3, behavior:'smooth'});
+	img3.style.borderColor = "#f5c181";
+})
+
+img4.addEventListener('click' , () =>{
+	window.scrollTo({top:sec4, behavior:'smooth'});
+	img4.style.borderColor = "#f5c181";
+})
+
+img5.addEventListener('click' , () =>{
+	window.scrollTo({top:sec5, behavior:'smooth'});
+	img5.style.borderColor = "#f5c181";
+})
+
+img6.addEventListener('click' , () =>{
+	window.scrollTo({top:sec6, behavior:'smooth'});
+	img6.style.borderColor = "#f5c181";
+})
+
+img7.addEventListener('click' , () =>{
+	window.scrollTo({top:sec7, behavior:'smooth'});
+	img7.style.borderColor = "#f5c181";
+})
+
+img8.addEventListener('click' , () =>{
+	window.scrollTo({top:sec8, behavior:'smooth'});
+	img8.style.borderColor = "#f5c181";
+})
+
+img10.addEventListener('click' , () =>{
+	window.scrollTo({top:header, behavior:'smooth'});
+	img9.style.Color = "#f5c181";
+})
