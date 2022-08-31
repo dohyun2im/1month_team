fe80f708322eb0d6d7b91c42eb24251334ca92c5
504 <504@DESKTOP-ECPN2C5>
2022-08-31 16:31:02 +0900

    마지막 완료

--- team1month/public/project_team/1month.js
+++ team1month/public/project_team/1month.js
@@ -1,3 +1,41 @@
+function imgmodal( num , clicknum ){
+	document.querySelector('#btnmodal').click() // 모달 실행 
+	
+	// 모든 세션들 호출 [ 인수로 들어온 번호의 해당하는 세션 ]
+	let section =  document.querySelectorAll('section')[num-1]
+	// 해당 세션에서 대표이미지 2개 
+	let fimenames = section.querySelectorAll('.imgbox > img');
+	// 해당 세션에서 이미지 4개 
+	let fimenames2 = section.querySelectorAll('.imgbox2 > img');
+	
+	let path = [ ] // 이미지들의 경로를 저장하는 배열 
+	
+	for( let i = 0 ; i<fimenames.length ; i++ ){ // 대표 이미지만큼 반복문 이용한 경로를 추출 -> 배열에 담기 
+		path.push( fimenames[i].src );
+	}
+	for( let i = 0 ; i<fimenames2.length ; i++ ){ //  // 이미지만큼 반복문 이용한 경로를 추출 -> 배열에 담기 
+		path.push( fimenames2[i].src );
+	}
+	
+	let carouselinner = document.querySelector('.carousel-inner'); // 모달 내용 div
+	
+	let output = '' 
+	// 배열내 이미지 경로만큼 반복문을 이용한 이미지슬라이드(캐러셀) div 생성 
+	for( let i = 0 ; i<path.length; i++ ){
+		if( clicknum == i ){
+			output += '<div class="carousel-item active">';
+		}else{
+			output += '<div class="carousel-item">';
+		}
+		output +=
+			'<img src="'+path[i]+'" class="d-block w-100" alt="..." >'+
+		'</div>';
+	}
+	carouselinner.innerHTML = output
+	
+	}
+
+
 let header = document.querySelector("header").offsetTop;
 let sec1 = document.querySelector(".one").offsetTop;
 let sec2 = document.querySelector(".two").offsetTop;
